af4947b304fdc72d716bb780e8b14a34588df35c
JohnathanFL <jfl747076@gmail.com>
2020-06-02 14:41:12 -0500

    Add a placeholder formatting file, make sqlite_orm work as a proper subproj

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,23 @@
 
 #include <gtkmm-3.0/gtkmm.h>
 
-#include "sqlite_orm.h"
+#include "sqlite_orm/sqlite_orm.h"
 
 using namespace std;
 using namespace sqlite_orm;
 
-struct Deck {
-  
-};
+struct Deck {};
 
-struct Material {
-  
-};
+struct Material {};
 
-struct Employee {
-  
-};
+struct Employee {};
 
 
 int main(int argc, char** argv) {
+  auto storage = make_storage("db.sqlite");
 
-  auto storage = make_storage("db.sqlite"
-  );
-  
-  auto app = Gtk::Application::create(argc, argv, "edu.mnstate.DeckEst");
-  auto builder = Gtk::Builder::create_from_file("ui/main.glade");
+  auto         app     = Gtk::Application::create(argc, argv, "edu.mnstate.DeckEst");
+  auto         builder = Gtk::Builder::create_from_file("ui/main.glade");
   Gtk::Window* win;
   builder->get_widget("window", win);
 
