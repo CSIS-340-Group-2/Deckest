37fc46339a6617e4008d9d9dbe0cd780eeccc560
JohnathanFL <jfl747076@gmail.com>
2020-06-02 14:08:47 -0500

    Add sqlite_orm and a skeleton for when we go over DB stuff tonight

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,29 @@
 
 #include <gtkmm-3.0/gtkmm.h>
 
+#include "sqlite_orm.h"
+
 using namespace std;
+using namespace sqlite_orm;
+
+struct Deck {
+  
+};
+
+struct Material {
+  
+};
+
+struct Employee {
+  
+};
+
 
 int main(int argc, char** argv) {
+
+  auto storage = make_storage("db.sqlite"
+  );
+  
   auto app = Gtk::Application::create(argc, argv, "edu.mnstate.DeckEst");
   auto builder = Gtk::Builder::create_from_file("ui/main.glade");
   Gtk::Window* win;
