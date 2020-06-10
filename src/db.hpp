213d7cda5dd126585c476122b310bf31fdbef334
JohnathanFL <jfl747076@gmail.com>
2020-06-10 16:05:23 -0500

    Reindent and tabs -> spaces

--- src/db.hpp
+++ src/db.hpp
@@ -19,13 +19,13 @@ struct Material {
 
   std::unique_ptr<double> length, width, height;
 
-  void update();
+  void               update();
   std::vector<Order> orders();
 };
 
 struct Order {
   int deckID, matID, quantity;
-  
+
   void update();
 };
 
@@ -33,15 +33,15 @@ struct Deck {
   int         id;
   std::string name, color;
   double      length, width, height;
-  
-  void update();
+
+  void               update();
   std::vector<Order> orders();
-  std::vector<Work> works();
+  std::vector<Work>  works();
 };
 
 struct Work {
   int deckID, empID, hours;
-  
+
   void update();
 };
 
@@ -50,8 +50,8 @@ struct Employee {
   std::string                  firstName, lastName;
   double                       wages;
   std::unique_ptr<std::string> phone;
-  
-  void update();
+
+  void              update();
   std::vector<Work> works();
 };
 
@@ -85,36 +85,36 @@ struct DB {
         make_column("width", &Deck::width),
         make_column("height", &Deck::height)
       ), 
-    	make_table("Employee", 
-    		make_column("id", &Employee::id, autoincrement(), primary_key()),
-    		make_column("firstName", &Employee::firstName),
-    		make_column("lastName", &Employee::lastName),
-    		make_column("wages", &Employee::wages),
-    		make_column("phone", &Employee::phone)
-    	),
-    	make_table("Work",
-    		make_column("deckID", &Work::deckID),
-    		make_column("empID", &Work::empID),
-    		make_column("hours", &Work::hours),
-		foreign_key(&Work::deckID).references(&Deck::id),
-		foreign_key(&Work::empID).references(&Employee::id)
-    	),
-    	make_table("Order",
-    		make_column("deckID", &Order::deckID),
-    		make_column("matID", &Order::matID),
-    		make_column("quantity", &Order::quantity),
-		foreign_key(&Order::deckID).references(&Deck::id),
-		foreign_key(&Order::matID).references(&Material::id)
-    	),
-    	make_table("Material", 
-    		make_column("id", &Material::id, autoincrement(), primary_key()),
-    		make_column("pricePerUnit", &Material::pricePerUnit),
-    		make_column("type", &Material::type),
-    		make_column("kind", &Material::kind),
-    		make_column("length", &Material::length),
-    		make_column("width", &Material::width),
-    		make_column("height", &Material::height)
-    	)
+      make_table("Employee", 
+        make_column("id", &Employee::id, autoincrement(), primary_key()),
+        make_column("firstName", &Employee::firstName),
+        make_column("lastName", &Employee::lastName),
+        make_column("wages", &Employee::wages),
+        make_column("phone", &Employee::phone)
+      ),
+      make_table("Work",
+        make_column("deckID", &Work::deckID),
+        make_column("empID", &Work::empID),
+        make_column("hours", &Work::hours),
+        foreign_key(&Work::deckID).references(&Deck::id),
+        foreign_key(&Work::empID).references(&Employee::id)
+      ),
+      make_table("Order",
+        make_column("deckID", &Order::deckID),
+        make_column("matID", &Order::matID),
+        make_column("quantity", &Order::quantity),
+        foreign_key(&Order::deckID).references(&Deck::id),
+        foreign_key(&Order::matID).references(&Material::id)
+      ),
+      make_table("Material", 
+        make_column("id", &Material::id, autoincrement(), primary_key()),
+        make_column("pricePerUnit", &Material::pricePerUnit),
+        make_column("type", &Material::type),
+        make_column("kind", &Material::kind),
+        make_column("length", &Material::length),
+        make_column("width", &Material::width),
+        make_column("height", &Material::height)
+      )
     );
     // clang-format on
     return storage;
