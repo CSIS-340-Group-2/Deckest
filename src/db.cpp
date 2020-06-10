b609842987c81ea168fdbf3fc75348bdc995c0fc
JohnathanFL <jfl747076@gmail.com>
2020-06-10 15:40:50 -0500

    Work on the public API

--- src/db.cpp
+++ src/db.cpp
@@ -1,1 +1,7 @@
 #include "db.hpp"
+
+void Material::update() { DB::get_db().replace(*this); }
+void Order::update() { DB::get_db().replace(*this); }
+void Deck::update() { DB::get_db().replace(*this); }
+void Work::update() { DB::get_db().replace(*this); }
+void Employee::update() { DB::get_db().replace(*this); }
--- src/db.hpp
+++ src/db.hpp
@@ -16,20 +16,28 @@ struct Material {
       kind;
 
   std::unique_ptr<double> length, width, height;
+
+  void update();
 };
 
 struct Order {
   int deckID, matID, quantity;
+  
+  void update();
 };
 
 struct Deck {
   int         id;
   std::string name, color;
   double      length, width, height;
+  
+  void update();
 };
 
 struct Work {
   int deckID, empID, hours;
+  
+  void update();
 };
 
 struct Employee {
@@ -37,14 +45,40 @@ struct Employee {
   std::string                  firstName, lastName;
   double                       wages;
   std::unique_ptr<std::string> phone;
+  
+  void update();
 };
 
-class DB {
- public:
- private:
-  static auto make_db() {
+struct DB {
+  //
+
+  static std::vector<Deck>     get_decks();
+  static std::vector<Material> get_materials();
+  static std::vector<Employee> get_employees();
+
+  static std::vector<Order> get_orders_for(int deckID);
+  static std::vector<Order> get_orders_of(int matID);
+
+  
+  static std::vector<Work>  get_working_on(int deckID);
+  static std::vector<Work>  get_jobs(int empID);
+
+
+  static std::vector<Material> get_mats_of_kind(const std::string& kind);
+  static std::vector<Material> get_mats_of_type(const std::string& type);
+
+
+  static Material get_material(int id);
+  static Order    get_order(int id);
+  static Deck     get_deck(int id);
+  // I've solved unemployment!
+  static Work     get_work(int id);
+  static Employee get_employee(int id);
+
+  static auto& get_db() {
     using namespace sqlite_orm;
-    return make_storage("deckest.db",
+    // clang-format off
+    static auto storage = make_storage("deckest.db",
       make_table("deck",
         make_column("id", &Deck::id, autoincrement(), primary_key()),
         make_column("name", &Deck::name),
@@ -54,5 +88,7 @@ class DB {
         make_column("height", &Deck::height)
       )
     );
+    // clang-format on
+    return storage;
   }
 };
