8dfe83b1bbefe96ddce360d984b9c2a6aae2110d
JohnathanFL <jfl747076@gmail.com>
2020-06-25 20:21:02 -0500

    Estimator stuff

--- src/estimator_ui.cpp
+++ src/estimator_ui.cpp
@@ -1,8 +1,29 @@
 #include <gtkmm-3.0/gtkmm.h>
 
+/// For displaying the stuff in "Name      :  $price" format
+struct CompCols: public Gtk::ListStore::ColumnRecord {
+ public:
+  Gtk::TreeModelColumn<std::string> colName;
+  Gtk::TreeModelColumn<std::string> colPrice;
+
+  CompCols() {
+    add(colName);
+    add(colPrice);
+  }
+};
+
 void create_estimator(Gtk::Builder* builder, int deckID) {
+  static CompCols compCols;
   Gtk::Window* window;
   builder->get_widget("root", window);
+
+  Gtk::TreeView* treeView;
+  builder->get_widget("compList", treeView);
+  auto model = treeView->get_model();
+
+
+
+  
   builder->get_application()->add_window(*window);
   window->show();
 }
